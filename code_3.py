8d85a6cab92031c28b863e770dadcad883622c42
evgenwebmaster23 <161200325+evgenwebmaster23@users.noreply.github.com>
2024-03-29 17:09:54 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = generateRandomData();
+
+
+    const data = [];
+    return data;
+}
+    }
+        console.log("Random Number: " + item);
+main();
+    data.forEach(item => {
+    });
+}
+function generateRandomData() {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
